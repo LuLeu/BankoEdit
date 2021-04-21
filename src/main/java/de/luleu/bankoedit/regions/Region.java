b8f6d18d7429301dfc2789346b64bfd301df2cfe
derLuLeu <derluleu@gmail.com>
2021-04-21 22:05:01 +0200

    Add function getWorld and setWorld

--- src/main/java/de/luleu/bankoedit/regions/Region.java
+++ src/main/java/de/luleu/bankoedit/regions/Region.java
@@ -1,6 +1,8 @@
 package de.luleu.bankoedit.regions;
 
 import de.luleu.bankoedit.math.BlockVector;
+import org.bukkit.World;
+import org.jetbrains.annotations.Nullable;
 
 public interface Region extends Iterable<BlockVector>, Cloneable {
 
@@ -17,6 +19,20 @@ public interface Region extends Iterable<BlockVector>, Cloneable {
      * @return max. point
      */
     BlockVector getSecondVector();
+
+    /**
+     * Sets the world that the selection is in.
+     *
+     * @return the world, or null
+     */
+    @Nullable World getWorld();
+
+    /**
+     * Sets the world that the selection is in.
+     *
+     * @param world the world, which may be null
+     */
+    void setWorld(@Nullable World world);
 
     /**
      * Returns true based on whether the region contains the point.
