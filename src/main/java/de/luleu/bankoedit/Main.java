f97ce349f37a95db9880824bfdec78531b1c9e02
derLuLeu <derluleu@gmail.com>
2021-05-13 17:08:46 +0200

    remove unused function onCommand and remove used function onCommand

--- src/main/java/de/luleu/bankoedit/Main.java
+++ src/main/java/de/luleu/bankoedit/Main.java
@@ -1,67 +1,23 @@
 package de.luleu.bankoedit;
 
-import de.luleu.bankoedit.regions.Region;
-import de.luleu.bankoedit.regions.impl.RegionImpl;
-import org.bukkit.Material;
-import org.bukkit.block.Block;
-import org.bukkit.command.Command;
+import de.luleu.bankoedit.command.SessionCommand;
+import de.luleu.bankoedit.command.SetCommand;
+import de.luleu.bankoedit.sessions.SessionManager;
 import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Main extends JavaPlugin implements CommandExecutor {
+public class Main extends JavaPlugin implements CommandExecutor {
+
+    public static SessionManager sessionManager;
 
     @Override
     public void onEnable() {
-        getCommand("mark").setExecutor(this);
+        sessionManager = new SessionManager();
+
+        this.getCommand("set").setExecutor(new SetCommand());
+        this.getCommand("set").setTabCompleter(new SetCommand());
+
+        this.getCommand("session").setExecutor(new SessionCommand());
+        this.getCommand("session").setTabCompleter(new SessionCommand());
     }
-
-    @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        Player player = (Player) sender;
-
-        Region region = new RegionImpl(
-                player.getWorld(),
-                player.getLocation().getBlockX(),
-                player.getLocation().getBlockY(),
-                player.getLocation().getBlockZ(),
-                player.getLocation().getBlockX() + 10,
-                player.getLocation().getBlockY() + 50,
-                player.getLocation().getBlockZ() + 10);
-
-
-        region.forEach(blockVector -> {
-            Block block = region.getWorld().getBlockAt(blockVector.getX(), blockVector.getY(), blockVector.getZ());
-
-            block.setType(Material.STONE);
-        });
-
-        return false;
-    }
-
-    //@Override
-    //public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
-    //    Player player = (Player) sender;
-
-    //    RegionSelector selector = new RegionSelectorImpl(player.getWorld());
-
-    //    selector.selectPrimary(BlockVector.at(player.getLocation().getBlockX(),
-    //            player.getLocation().getBlockY(),
-    //            player.getLocation().getBlockZ()));
-
-    //    selector.selectSecondary(BlockVector.at(player.getLocation().getBlockX() + 10,
-    //            player.getLocation().getBlockY() + 50,
-    //            player.getLocation().getBlockZ() + 10));
-
-
-    //    selector.getRegion().forEach(blockVector -> {
-    //        Block block = selector.getRegion().getWorld().getBlockAt(blockVector.getX(), blockVector.getY(), blockVector.getZ());
-
-    //        block.setType(Material.STONE);
-    //    });
-
-    //    return false;
-    //}
 }
-
